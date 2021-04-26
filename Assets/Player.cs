e4103591e205dd59f8a152f2ad326c23ef55bd4c
Danit <danishagan@gmail.com>
2021-04-26 16:45:14 +0300

    Update project
    * feat: Add score to the player
    * feat: Add disqualification possibility

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,11 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Player : MonoBehaviour
 {
     public Rigidbody rb;
     public float jumpPower = 5;
+    public int score = 0;
+    public TextMeshProUGUI scoreText;
 
     // Start is called before the first frame update
     void Start()
@@ -20,5 +23,21 @@ public class Player : MonoBehaviour
         {
             rb.velocity += Vector3.up * jumpPower;
         }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "AddScore")
+        {
+            score++;
+            scoreText.text = score.ToString();
+        }
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "Obs")
+        Application.LoadLevel(Application.loadedLevel);
     }
 }
